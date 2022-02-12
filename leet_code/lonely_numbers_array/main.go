cc48477278346b2d4155a06ce1fa12b031605ec9
Michel Aquino <michelaquino@mailbox.org>
2022-02-12 14:55:49 -0300

    [leet code] Lonely numbers array

---
+++ leet_code/lonely_numbers_array/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := []int{62, 35, 59, 55, 84, 61, 38, 87, 55, 82}
+
+	output := findLonely(input)
+	fmt.Println(output)
+}
+
+func findLonely(nums []int) []int {
+	mapCount := map[int]int{}
+
+	for _, num := range nums {
+		mapCount[num] += 1
+	}
+
+	result := []int{}
+	for number, count := range mapCount {
+		if count > 1 {
+			continue
+		}
+
+		if _, found := mapCount[number+1]; found {
+			continue
+		}
+
+		if _, found := mapCount[number-1]; found {
+			continue
+		}
+
+		result = append(result, number)
+	}
+
+	return result
+}
---
+++ leet_code/lonely_numbers_array/problem.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/find-all-lonely-numbers-in-the-array/

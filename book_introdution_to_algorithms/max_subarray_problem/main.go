92b18cf279e8a280d4993e77ad2e1fe09ea16615
Michel Aquino <michelaquino@mailbox.org>
2022-02-13 17:32:24 -0300

    Brute force of max sub array

--- book_introdution_to_algorithms/max_subarray_problem/main.go
+++ book_introdution_to_algorithms/max_subarray_problem/main.go
@@ -9,21 +9,42 @@ func main() {
 	input := []int{100, 113, 110, 85, 105, 102, 86, 63, 81, 101, 94, 106, 101, 79, 94, 90, 97}
 
 	leftIndex, rightIndex, totalSum := findMaxSubarray(input, 0, len(input)-1)
+	// leftIndex, rightIndex, totalSum := findMaxSubarrayBruteForce(input)
 	fmt.Println(leftIndex)
 	fmt.Println(rightIndex)
 	fmt.Println(totalSum)
 
 }
 
-func findMaxSubarray(input []int, lowIndex, highIndex int) (leftIndex int, rightIndex, totalSum int) {
-	diferences := make([]int, len(input))
-	for i, n := range input {
-		if i == 0 {
-			continue
-		}
+func findMaxSubarrayBruteForce(input []int) (leftIndex int, rightIndex, totalSum int) {
+	diferences := buildDiferences(input)
+	var max float64 = math.Inf(-1)
 
-		diferences[i] = n - input[i-1]
+	for i := 0; i < len(diferences); i++ {
+		var sum float64 = float64(diferences[i])
+		fmt.Println()
+
+		for j := i + 1; j < len(diferences); j++ {
+			sum += float64(diferences[j])
+
+			if sum > max {
+				fmt.Printf("sum: %f; max %f\n", sum, max)
+				max = sum
+
+				fmt.Printf("i: %d => %d\n", i, diferences[i])
+				fmt.Printf("j: %d => %d\n", j, diferences[j])
+				leftIndex = i
+				rightIndex = j
+			}
+		}
 	}
+
+	totalSum = int(max)
+	return
+}
+
+func findMaxSubarray(input []int, lowIndex, highIndex int) (leftIndex int, rightIndex, totalSum int) {
+	diferences := buildDiferences(input)
 
 	// base case
 	if lowIndex == highIndex {
@@ -36,7 +57,7 @@ func findMaxSubarray(input []int, lowIndex, highIndex int) (leftIndex int, right
 
 	leftLow, leftHigh, leftSum := findMaxSubarray(input, lowIndex, midIndex)
 	rightLow, rightHigh, rightSum := findMaxSubarray(input, midIndex+1, highIndex)
-	crossLow, crossHigh, crossSum := findMaxCrossingSubarray(diferences, lowIndex, midIndex, highIndex)
+	crossLow, crossHigh, crossSum := findMaxCrossingSubarray(input, lowIndex, midIndex, highIndex)
 	if leftSum > rightHigh && leftSum > crossSum {
 		leftIndex = leftLow
 		rightIndex = leftHigh
@@ -59,13 +80,15 @@ func findMaxSubarray(input []int, lowIndex, highIndex int) (leftIndex int, right
 }
 
 func findMaxCrossingSubarray(input []int, lowIndex, midIndex, highIndex int) (maxLeftIndex int, maxRightIndex, totalSum int) {
+	diferences := buildDiferences(input)
+
 	maxLeftIndex = -1
 	maxRightIndex = -1
 
 	var leftTotalSum float64 = math.Inf(-1)
 	var sum float64 = 0
 	for i := midIndex; i >= lowIndex; i-- {
-		sum += float64(input[i])
+		sum += float64(diferences[i])
 
 		if sum > leftTotalSum {
 			leftTotalSum = sum
@@ -76,7 +99,7 @@ func findMaxCrossingSubarray(input []int, lowIndex, midIndex, highIndex int) (ma
 	var rightTotalSum float64 = math.Inf(-1)
 	sum = 0
 	for i := midIndex + 1; i <= highIndex; i++ {
-		sum += float64(input[i])
+		sum += float64(diferences[i])
 
 		if sum > rightTotalSum {
 			rightTotalSum = sum
@@ -86,4 +109,17 @@ func findMaxCrossingSubarray(input []int, lowIndex, midIndex, highIndex int) (ma
 
 	totalSum = int(leftTotalSum + rightTotalSum)
 	return
+}
+
+func buildDiferences(input []int) []int {
+	diferences := make([]int, len(input))
+	for i, n := range input {
+		if i == 0 {
+			continue
+		}
+
+		diferences[i] = n - input[i-1]
+	}
+
+	return diferences
 }

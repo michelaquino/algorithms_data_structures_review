12cc464508fa376f9df487fc5b3934ed5b4813f1
Michel Aquino <michelaquino@mailbox.org>
2022-04-05 21:31:30 -0300

    [CLRS] Heapsort - max heapify method

---
+++ books/clrs/chapter-6/heapsort/max_hapfify.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+type heap struct {
+	array  []int
+	size   int
+	length int
+}
+
+func newHeap(array []int, startIndex, endIndex int) *heap {
+	return &heap{
+		array:  array,
+		length: len(array),
+		size:   endIndex - startIndex,
+	}
+}
+
+func (h *heap) exchangePosition(position1, position2 int) {
+	value1 := h.array[position1]
+	h.array[position1] = h.array[position2]
+	h.array[position2] = value1
+}
+
+func (h *heap) print() {
+	fmt.Println()
+	for _, a := range h.array {
+		fmt.Printf("%d ", a)
+	}
+	fmt.Println()
+}
+
+func main() {
+	array := []int{16, 4, 10, 14, 7, 9, 3, 2, 8, 1}
+	heap := newHeap(array, 0, len(array))
+
+	heap.print()
+	maxHeapify(heap, 1)
+	heap.print()
+}
+
+func maxHeapify(heap *heap, position int) {
+	left := left(position)
+	right := right(position)
+	largest := -1
+
+	if left < heap.size && heap.array[left] > heap.array[position] {
+		largest = left
+	} else {
+		largest = position
+	}
+
+	if right < heap.size && heap.array[right] > heap.array[largest] {
+		largest = right
+	}
+
+	if largest != position {
+		heap.exchangePosition(largest, position)
+		maxHeapify(heap, largest)
+	}
+}
+
+func parent(i int) int {
+	return i >> 1
+}
+
+func left(i int) int {
+	return (i << 1) + 1
+}
+
+func right(i int) int {
+	return (i << 1) + 2
+}

a103a4e7caa41ddbe1507d6244afa7eef1275573
Michel Aquino <michelaquino@mailbox.org>
2022-04-06 20:12:20 -0300

    [CLRS] Heapsort - move max-heapify and build-max-heap to heap

--- books/clrs/chapter-6/heap/heap.go
+++ books/clrs/chapter-6/heap/heap.go
@@ -22,6 +22,36 @@ func (h *Heap) ExchangePosition(position1, position2 int) {
 	h.array[position2] = value1
 }
 
+func (h *Heap) MaxHeapify(position int) {
+	left := left(position)
+	right := right(position)
+	largest := -1
+
+	if left <= h.Size() && h.Array()[left] > h.Array()[position] {
+		largest = left
+	} else {
+		largest = position
+	}
+
+	if right <= h.Size() && h.Array()[right] > h.Array()[largest] {
+		largest = right
+	}
+
+	if largest != position {
+		h.ExchangePosition(largest, position)
+		h.MaxHeapify(largest)
+	}
+}
+
+func (h *Heap) BuildMaxHeap() {
+	h.SetLengthToSize()
+
+	middle := h.Length() / 2
+	for i := middle; i >= 0; i-- {
+		h.MaxHeapify(i)
+	}
+}
+
 func (h Heap) Print() {
 	fmt.Println()
 	for _, a := range h.array {
@@ -48,4 +78,16 @@ func (h Heap) Length() int {
 
 func (h Heap) Size() int {
 	return h.size
+}
+
+func parent(i int) int {
+	return i >> 1
+}
+
+func left(i int) int {
+	return (i << 1) + 1
+}
+
+func right(i int) int {
+	return (i << 1) + 2
 }
--- books/clrs/chapter-6/heapsort/heapsort.go
+++ books/clrs/chapter-6/heapsort/heapsort.go
@@ -4,55 +4,13 @@ import "clrs_chapter_6/heap"
 
 func Heapsort(array []int) []int {
 	heap := heap.NewHeap(array, 0, len(array))
-	buildMaxHeap(heap)
+	heap.BuildMaxHeap()
 
 	for i := heap.Length(); i >= 1; i-- {
 		heap.ExchangePosition(0, i)
 		heap.DownHeapSize()
-		maxHeapify(heap, 0)
+		heap.MaxHeapify(0)
 	}
 
 	return heap.Array()
-}
-
-func maxHeapify(heap *heap.Heap, position int) {
-	left := left(position)
-	right := right(position)
-	largest := -1
-
-	if left <= heap.Size() && heap.Array()[left] > heap.Array()[position] {
-		largest = left
-	} else {
-		largest = position
-	}
-
-	if right <= heap.Size() && heap.Array()[right] > heap.Array()[largest] {
-		largest = right
-	}
-
-	if largest != position {
-		heap.ExchangePosition(largest, position)
-		maxHeapify(heap, largest)
-	}
-}
-
-func buildMaxHeap(heap *heap.Heap) {
-	heap.SetLengthToSize()
-
-	middle := heap.Length() / 2
-	for i := middle; i >= 0; i-- {
-		maxHeapify(heap, i)
-	}
-}
-
-func parent(i int) int {
-	return i >> 1
-}
-
-func left(i int) int {
-	return (i << 1) + 1
-}
-
-func right(i int) int {
-	return (i << 1) + 2
 }

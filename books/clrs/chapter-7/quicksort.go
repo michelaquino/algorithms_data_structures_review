30f0a9a8eb18661b39e44f319d827a2bf65eedd2
Michel Aquino <michelaquino@mailbox.org>
2022-04-15 13:19:00 -0300

    [CLRS] Randomized Quicksort

--- books/clrs/chapter-7/quicksort.go
+++ books/clrs/chapter-7/quicksort.go
@@ -1,13 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	array := []int{5, 7, 2, 1, 0, 8, 3, 6, 9, 4}
 
 	fmt.Println(array)
-	arraySorted := quicksort(array, 0, len(array)-1)
+	arraySorted := randomizedQuicksort(array, 0, len(array)-1)
 	fmt.Println(arraySorted)
+}
+
+func randomizedQuicksort(arrayToSort []int, startIndex, endIndex int) []int {
+	array := make([]int, len(arrayToSort))
+	copy(array, arrayToSort)
+
+	if startIndex >= endIndex {
+		return array
+	}
+
+	middle, arrayPartitioned := randomizedPartition(array, startIndex, endIndex)
+	leftSorted := randomizedQuicksort(arrayPartitioned, startIndex, middle-1)
+	rightSorted := randomizedQuicksort(leftSorted, middle+1, endIndex)
+	return rightSorted
 }
 
 func quicksort(arrayToSort []int, startIndex, endIndex int) []int {
@@ -51,4 +69,17 @@ func partition(array []int, startIndex, endIndex int) (int, []int) {
 	arrayCopy[endIndex] = arrayCopy[i+1]
 	arrayCopy[i+1] = pivot
 	return i + 1, arrayCopy
+}
+
+func randomizedPartition(array []int, startIndex, endIndex int) (int, []int) {
+	arrayCopy := make([]int, len(array))
+	copy(arrayCopy, array)
+
+	rand.Seed(time.Now().UnixNano())
+	randIndex := startIndex + rand.Intn(endIndex-startIndex)
+	aux := arrayCopy[endIndex]
+	arrayCopy[endIndex] = arrayCopy[randIndex]
+	arrayCopy[randIndex] = aux
+
+	return partition(arrayCopy, startIndex, endIndex)
 }

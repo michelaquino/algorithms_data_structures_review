3597d6e140e3646fc16753d2c7c326c2c597d9b6
Michel Aquino <michelaquino@mailbox.org>
2022-12-21 21:16:43 -0300

    [leetcode] 256. Paint House. Dynamic Programming

--- trainning/leet_code/256-paint-house/main.go
+++ trainning/leet_code/256-paint-house/main.go
@@ -11,22 +11,22 @@ func main() {
 		costs         [][]int
 		expectdOutput int
 	}{
-		// {
-		// 	costs:         [][]int{{17, 2, 17}, {16, 16, 5}, {14, 3, 19}},
-		// 	expectdOutput: 10,
-		// },
-		// {
-		// 	costs:         [][]int{{7, 6, 2}},
-		// 	expectdOutput: 2,
-		// },
-		// {
-		// 	costs:         [][]int{},
-		// 	expectdOutput: 0,
-		// },
-		// {
-		// 	costs:         [][]int{{5, 8, 6}, {19, 14, 13}, {7, 5, 12}, {14, 15, 17}, {3, 20, 10}},
-		// 	expectdOutput: 43,
-		// },
+		{
+			costs:         [][]int{{17, 2, 17}, {16, 16, 5}, {14, 3, 19}},
+			expectdOutput: 10,
+		},
+		{
+			costs:         [][]int{{7, 6, 2}},
+			expectdOutput: 2,
+		},
+		{
+			costs:         [][]int{},
+			expectdOutput: 0,
+		},
+		{
+			costs:         [][]int{{5, 8, 6}, {19, 14, 13}, {7, 5, 12}, {14, 15, 17}, {3, 20, 10}},
+			expectdOutput: 43,
+		},
 		{
 			costs:         [][]int{{8, 12, 18}, {14, 6, 8}, {10, 9, 13}, {2, 17, 14}, {18, 18, 6}, {2, 1, 15}, {19, 20, 2}, {18, 15, 16}, {20, 18, 18}, {15, 10, 10}, {2, 20, 18}, {14, 5, 15}, {18, 10, 12}, {9, 17, 19}, {9, 1, 6}, {4, 4, 10}, {7, 8, 15}, {16, 17, 4}, {16, 16, 13}, {12, 7, 10}, {14, 13, 8}, {16, 6, 18}, {10, 5, 10}, {3, 5, 11}, {9, 9, 6}, {10, 15, 19}, {4, 5, 19}, {12, 17, 17}},
 			expectdOutput: 43,
@@ -44,7 +44,23 @@ func main() {
 	}
 }
 
+// Dynamic programming
 func minCost(costs [][]int) int {
+	if len(costs) == 0 {
+		return 0
+	}
+
+	for i := len(costs) - 2; i >= 0; i-- {
+		costs[i][0] = costs[i][0] + min(costs[i+1][1], costs[i+1][2])
+		costs[i][1] = costs[i][1] + min(costs[i+1][0], costs[i+1][2])
+		costs[i][2] = costs[i][2] + min(costs[i+1][0], costs[i+1][1])
+	}
+
+	return min(costs[0][0], min(costs[0][1], costs[0][2]))
+}
+
+// Recursive with memo
+func minCost_tree(costs [][]int) int {
 	memo := map[string]int{}
 
 	var cost func(currentHouse, currentCostIndex int) int

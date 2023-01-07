c674d16614ea7bafaee005c73888cd423e954c40
Michel Aquino <michelaquino@mailbox.org>
2023-01-07 16:00:50 -0300

    [leetcode] 17. Letter Combinations of a Phone Number

--- trainning/leet_code/17-letter-combinations-of-a-phone-number/main.go
+++ trainning/leet_code/17-letter-combinations-of-a-phone-number/main.go
@@ -2,45 +2,69 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-var (
-	numberLetterMap = map[string][]string{
-		"0": {" "},
-		"2": {"a", "b", "c"},
-		"3": {"d", "e", "f"},
-		"4": {"g", "h", "i"},
-		"5": {"j", "k", "l"},
-		"6": {"m", "n", "o"},
-		"7": {"p", "q", "r", "s"},
-		"8": {"t", "u", "v"},
-		"9": {"w", "x", "y", "z"},
-	}
-)
-
 func main() {
-	letterCombinations("234")
-}
-
-func letterCombinations(digits string) {
-	resultList := bfs(digits, "", 0)
-	fmt.Println(resultList)
-}
-
-func bfs(digits string, parcialResult string, level int) []string {
-	if level == len(digits) {
-		return []string{parcialResult}
+	testCases := []struct {
+		digits        string
+		expectdOutput []string
+	}{
+		{
+			digits: "23",
+			expectdOutput: []string{
+				"ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf",
+			},
+		},
 	}
 
-	digit := string(digits[level])
-	letters := numberLetterMap[digit]
+	for _, t := range testCases {
+		fmt.Println("=========================================")
+		output := letterCombinations(t.digits)
+		fmt.Println("output: ", output)
 
-	resultList := []string{}
-	for _, letter := range letters {
-		result := bfs(digits, parcialResult+letter, level+1)
-		resultList = append(resultList, result...)
+		if !reflect.DeepEqual(output, t.expectdOutput) {
+			fmt.Println()
+			panic(fmt.Sprintf("\nexpcted %v, \ngot %v", t.expectdOutput, output))
+		}
+	}
+}
+
+func letterCombinations(digits string) []string {
+	numberLetterMap := map[rune][]rune{
+		'2': {'a', 'b', 'c'},
+		'3': {'d', 'e', 'f'},
+		'4': {'g', 'h', 'i'},
+		'5': {'j', 'k', 'l'},
+		'6': {'m', 'n', 'o'},
+		'7': {'p', 'q', 'r', 's'},
+		'8': {'t', 'u', 'v'},
+		'9': {'w', 'x', 'y', 'z'},
+		'0': {' '},
 	}
 
-	return resultList
+	answer := []string{}
+
+	var backtracking func(digits string, index int, currentComb string)
+	backtracking = func(digits string, index int, currentComb string) {
+		if index >= len(digits) {
+			if currentComb != "" {
+				answer = append(answer, currentComb)
+			}
+
+			return
+		}
+
+		number := digits[index]
+		for _, letter := range numberLetterMap[rune(number)] {
+
+			currentComb += string(letter)
+			backtracking(digits, index+1, currentComb)
+			currentComb = currentComb[:len(currentComb)-1]
+		}
+	}
+
+	backtracking(digits, 0, "")
+	return answer
 }

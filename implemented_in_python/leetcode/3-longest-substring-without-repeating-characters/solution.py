03483f483136f6267d0a7b2e5086f1c34818365a
Michel Aquino <michelaquino@mailbox.org>
2024-05-25 11:40:20 -0300

    [leetcode] Add a simpler solution
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- implemented_in_python/leetcode/3-longest-substring-without-repeating-characters/solution.py
+++ implemented_in_python/leetcode/3-longest-substring-without-repeating-characters/solution.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         if len(s) < 2:
@@ -12,20 +13,37 @@ class Solution:
         while second < len(s):
             count = letterCount.get(s[second], 0)
             letterCount[s[second]] = count + 1
-            
+
             if count > 0:
                 longest = max(longest, second - first)
                 while s[first] != s[second]:
                     letterCount[s[first]] -= 1
                     first += 1
-                
+
                 letterCount[s[first]] -= 1
                 first += 1
-            
+
             second += 1
-        
+
         return max(longest, second - first)
-        
+
+    def simpler_lengthOfLongestSubstring(self, s: str) -> int:
+        letterSet = set()
+        longest = 0
+        first, second = 0, 0
+
+        while second < len(s):
+            while s[second] in letterSet:
+                letterSet.remove(s[first])
+                first += 1
+
+            letterSet.add(s[second])
+            longest = max(longest, second - first + 1)
+            second += 1
+
+        return longest
+
+
 s = Solution()
 
 

747b73d51b9534d0a3e4890812742357a41ab498
Michel Aquino <michelaquino@mailbox.org>
2024-05-30 15:52:48 -0300

    [leetcode] 76. Minimum Window Substring
    https://leetcode.com/problems/minimum-window-substring

---
+++ implemented_in_python/leetcode/0076-minimum-window-substring/solution.py
@@ -0,0 +1,94 @@
+# https://leetcode.com/problems/minimum-window-substring/
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+
+        tCount = {}
+        for letter in t:
+            tCount[letter] = tCount.get(letter, 0) + 1
+
+        start, end = 0, len(s) + 1
+
+        lettersUsed = 0
+        left, right = 0, 0
+        while right <= len(s):
+            rightLetter = s[right] if right < len(s) else s[right - 1]
+            # found a substring
+            if lettersUsed == len(t):
+                if right - left < end - start:
+                    start = left
+                    end = right
+
+                tCount[s[left]] += 1
+                if tCount[s[left]] > 0:
+                    lettersUsed -= 1
+
+                left += 1
+                continue
+
+            if right < len(s):
+                tCount[s[right]] = tCount.get(s[right], 0) - 1
+                if tCount[s[right]] >= 0:
+                    lettersUsed += 1
+
+            right += 1
+
+        if end > len(s):
+            return ""
+
+        return s[start:end]
+
+
+class NeetcodeSolution:
+    def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                # update our result
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                # pop from the left of our window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
+
+
+sol = Solution()
+
+s = "ADOBECODEBANC"
+t = "ABC"
+print(sol.minWindow(s, t))
+
+s = "a"
+t = "a"
+print(sol.minWindow(s, t))
+
+s = "a"
+t = "aa"
+print(sol.minWindow(s, t))
+
+s = "a"
+t = "b"
+print(sol.minWindow(s, t))

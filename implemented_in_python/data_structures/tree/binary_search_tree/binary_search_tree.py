f00d0c01804a8699455d41165186314979800d40
Michel Aquino <michelaquino@mailbox.org>
2024-07-06 15:40:53 -0300

    [data structures] Binary search tree

---
+++ implemented_in_python/data_structures/tree/binary_search_tree/binary_search_tree.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self, root):
+        self.root = root
+
+    def insert(self, node):
+        return None
+
+    def delete(self):
+        return None
+
+    def search(self, value):
+        return None
+
+    def printInOrder(self):
+        return None
+
+    def printPreOrder(self):
+        return None
+
+    def printPostOrder(self):
+        return None
+
+    def minimum(self):
+        return None
+
+    def maximum(self):
+        return None

18b91e3467700e8b5a6b17c591fe57b34b36e138
Michel Aquino <michelaquino@mailbox.org>
2024-07-06 15:43:30 -0300

    [binary search tree] Minimum

--- implemented_in_python/data_structures/tree/binary_search_tree/binary_search_tree.py
+++ implemented_in_python/data_structures/tree/binary_search_tree/binary_search_tree.py
@@ -28,7 +28,21 @@ class Tree:
         return None
 
     def minimum(self):
-        return None
+        return self._iterative_minimum(self.root)
+        # return self._recursive_minimum(self.root)
+
+    def _recursive_minimum(self, node):
+        if not node or not node.left:
+            return node
+
+        return self._recursive_minimum(node.left)
+
+    def _iterative_minimum(self, node):
+        curr = node
+        while curr and curr.left:
+            curr = curr.left
+
+        return curr
 
     def maximum(self):
         return self._iterative_maximum(self.root)
@@ -46,3 +60,31 @@ class Tree:
             return node
 
         return self._recursive_maximum(node.right)
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+node6 = Node(6)
+node7 = Node(7)
+node9 = Node(9)
+
+
+node2.left = node1
+node2.right = node3
+
+node7.left = node6
+node7.right = node9
+
+node4.left = node2
+node4.right = node7
+
+tree = Tree(node4)
+
+minNode = tree.minimum()
+print("Min node: ", minNode.value if minNode else None)
+
+maxNode = tree.maximum()
+print("Max node: ", maxNode.value if maxNode else None)

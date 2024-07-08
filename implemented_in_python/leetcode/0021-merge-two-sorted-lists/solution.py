598006625d93300a5f47d5c862a091c12d944340
Michel Aquino <michelaquino@mailbox.org>
2024-07-08 09:21:57 -0300

    [leetcode] 21. Merge Two Sorted Lists
    A simpler iterative version
    
    https://leetcode.com/problems/merge-two-sorted-lists

--- implemented_in_python/leetcode/0021-merge-two-sorted-lists/solution.py
+++ implemented_in_python/leetcode/0021-merge-two-sorted-lists/solution.py
@@ -10,36 +10,24 @@ class Solution:
     # T: O(l1 + l2) M: O(1)
     def mergeTwoLists(self, list1, list2):
         curr1, curr2 = list1, list2
-        newHead = curr = None
-
-        def updateCurr(nextNode):
-            nonlocal curr
-            nonlocal newHead
-
-            if not curr:
-                curr = nextNode
-                newHead = curr
-                return
-
-            curr.next = nextNode
-            curr = curr.next
+        newHead = ListNode()
+        currHead = newHead
 
         while curr1 or curr2:
-            if not curr2:
-                updateCurr(curr1)
-                curr1 = curr1.next
-            elif not curr1:
-                updateCurr(curr2)
+            if not curr1:
+                currHead.next = curr2
                 curr2 = curr2.next
+            elif not curr2:
+                currHead.next = curr1
+                curr1 = curr1.next
+            elif curr1.val < curr2.val:
+                currHead.next = curr1
+                curr1 = curr1.next
             else:
-                if curr1.val < curr2.val:
-                    updateCurr(curr1)
-                    curr1 = curr1.next
-                else:
-                    updateCurr(curr2)
-                    curr2 = curr2.next
+                currHead.next = curr2
+                curr2 = curr2.next
 
-        return newHead
+            currHead = currHead.next
 
     # Recursive
     # T: O(l1 + l2) M: O(l1 + l2)

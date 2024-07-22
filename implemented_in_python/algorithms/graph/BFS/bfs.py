d00c2ca36b333c42ea9f1ecadc7438d014d8f30d
Michel Aquino <michelaquino@mailbox.org>
2024-07-22 09:58:33 -0300

    [graph] BFS

---
+++ implemented_in_python/algorithms/graph/BFS/bfs.py
@@ -0,0 +1,91 @@
+class Graph:
+    def __init__(self):
+        self.nodes = []
+
+    def insert_edge(self, node):
+        if not node:
+            return
+
+        self.nodes.append(node)
+
+    def search(self, value):
+        print("-----------------------")
+        if len(self.nodes) == 0:
+            return None
+
+        return self.bfs(value)
+
+    def bfs(self, value):
+        queue = [self.nodes[0]]
+        visited = set()
+
+        while len(queue) > 0:
+            node = queue.pop(0)
+            if node.value in visited:
+                continue
+
+            print(node.value)
+            if node.value == value:
+                return node
+
+            visited.add(node.value)
+
+            queue += node.neighboors
+
+        return None
+
+    def print(self):
+        print(self.nodes)
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.neighboors = []
+
+    def connectTo(self, node):
+        if not node:
+            return
+
+        self.neighboors.append(node)
+
+
+n0 = Node(0)
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+
+n0.connectTo(n2)
+n1.connectTo(n2)
+n1.connectTo(n3)
+n1.connectTo(n4)
+n2.connectTo(n0)
+n2.connectTo(n1)
+n2.connectTo(n5)
+n3.connectTo(n1)
+n4.connectTo(n1)
+n5.connectTo(n2)
+
+
+g = Graph()
+g.insert_edge(n0)
+g.insert_edge(n1)
+g.insert_edge(n2)
+g.insert_edge(n3)
+g.insert_edge(n4)
+g.insert_edge(n5)
+
+print("Found: ", g.search(1))
+print("Found: ", g.search(2))
+print("Found: ", g.search(3))
+print("Found: ", g.search(4))
+print("Found: ", g.search(5))
+print("Found: ", g.search(6))
+
+# n6 = Node(0)
+# n7 = Node(0)
+# n8 = Node(0)
+# n8 = Node(0)
+# n8 = Node(0)

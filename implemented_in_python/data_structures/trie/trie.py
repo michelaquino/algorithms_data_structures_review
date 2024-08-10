1b7993d7697842f2bf0e78bbe520f3ce1930031d
Michel Aquino <michelaquino@mailbox.org>
2024-08-10 17:33:47 -0300

    [leetcode] Simpler Trie

--- implemented_in_python/data_structures/trie/trie.py
+++ implemented_in_python/data_structures/trie/trie.py
@@ -1,64 +1,39 @@
-class Node:
+class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.terminal = False
+        self.children = [None] * 26
+        self.end = False
 
 
-class Trie:
+class SimplerTrie:
     def __init__(self):
-        self.root = None
+        self.root = TrieNode()
 
-    def insert(self, word):
-        if not self.root:
-            self.root = Node()
-
+    def insert(self, word: str) -> None:
         curr = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if curr.children[i] == None:
+                curr.children[i] = TrieNode()
+            curr = curr.children[i]
+        curr.end = True
 
-        lastLetter = ""
-        for i, char in enumerate(word):
-            lastLetter = char
-            if char not in curr.children:
-                node = Node()
-                curr.children[char] = node
-
-            curr = curr.children[char]
-
-        curr.terminal = True
-
-    def search(self, word):
-        def dfs(root, word, wordIndex):
-            if not root:
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if curr.children[i] == None:
                 return False
+            curr = curr.children[i]
+        return curr.end
 
-            if wordIndex == len(word) and root.terminal:
-                return True
-
-            letter = word[wordIndex] if wordIndex < len(word) else ""
-            if letter not in root.children:
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            i = ord(c) - ord("a")
+            if curr.children[i] == None:
                 return False
-
-            child = root.children[letter]
-            return dfs(child, word, wordIndex + 1)
-
-        return dfs(self.root, word, 0)
-
-    def startsWith(self, prefix):
-        def dfs(root, word, wordIndex):
-            if not root:
-                return False
-
-            if wordIndex >= len(word):
-                return True
-
-            letter = word[wordIndex]
-
-            if letter not in root.children:
-                return False
-
-            child = root.children[letter]
-            return dfs(child, word, wordIndex + 1)
-
-        return dfs(self.root, prefix, 0)
+            curr = curr.children[i]
+        return True
 
     def print(self):
         if not self.root:

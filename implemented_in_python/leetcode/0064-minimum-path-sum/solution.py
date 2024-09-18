001807705b5916781f77adf6f24d4184bc4c1fc3
Michel Aquino <michelaquino@mailbox.org>
2024-09-18 10:06:16 -0300

    [leetcode] 64. Minimum Path Sum
    https://leetcode.com/problems/minimum-path-sum

--- implemented_in_python/leetcode/0064-minimum-path-sum/solution.py
+++ implemented_in_python/leetcode/0064-minimum-path-sum/solution.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/minimum-path-sum
 # O(N * M) time
 # O(N * M) memory
-class Solution:
+class Solution_DFS_with_memo:
     def minPathSum(self, grid: List[List[int]]) -> int:
         default_value = float("infinity")
         memo = {}
@@ -24,3 +24,26 @@ class Solution:
             return result
 
         return dfs(0, 0)
+
+
+# DP
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        default_value = float("infinity")
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if row == 0 and col == 0:
+                    continue
+
+                top = float("infinity")
+                left = float("infinity")
+                if row > 0:
+                    top = grid[row - 1][col]
+
+                if col > 0:
+                    left = grid[row][col - 1]
+
+                grid[row][col] += min(top, left)
+
+        return grid[len(grid) - 1][len(grid[0]) - 1]
